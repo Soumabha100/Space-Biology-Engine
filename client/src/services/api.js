df04c5ead2afd83c48d4d36e5a3464922afb5f97
ServerDeveloper9447 <soumalyaghosh2022@gmail.com>
2025-10-05 21:54:41 +0530

    Updated schema

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://sbke.vercel.app";
+const API_BASE_URL = "https://sbke.vercel.app";
 
 export const getEntityData = async (entityId) => {
   if (!entityId) {
